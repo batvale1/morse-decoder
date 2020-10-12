6068cbee635b181ab106e0ac182acf96f99ad5f0
batvale1 <batvale1@yandex.ru>
2020-10-12 17:37:15 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arrOfWords = expr.split('**********');
+    arrOfWords.forEach((word, wordIndex) => {
+        let encodedLetterWithDashesAndDots = '';
+        let finalWord = '';
+        for (let i = 0; i < word.length; i += 10) {
+            let encodedLetter = word.slice(i, i + 10);
+            const firstMatterSymbol = encodedLetter.indexOf('1');
+            encodedLetter = encodedLetter.slice(firstMatterSymbol);
+            encodedLetterWithDashesAndDots = encodedLetter.replace(/10/g, '.');
+            encodedLetterWithDashesAndDots = encodedLetterWithDashesAndDots.replace(/11/gi, '-');
+            finalWord = finalWord + MORSE_TABLE[encodedLetterWithDashesAndDots];
+        }
+        arrOfWords[wordIndex] = finalWord;
+    });
+
+    return arrOfWords.join(' ');
+
 }
 
 module.exports = {
